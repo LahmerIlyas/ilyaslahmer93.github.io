ea5a8bdcbafad9e788cca3449603e8b1918fd4c3
lahmer ilyas <ilyaslahmer93@gmail.com>
2020-05-02 18:51:48 +0100

    template

---
+++ precache-manifest.e8d5e0ce590afc022b6e755d446bef68.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9cbe3e04f483176fcb6183d78a13f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5df9f1480fb6ecfa64",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "359700b17ed7ece44cf2",
+    "url": "/static/css/main.01dbdcf0.chunk.css"
+  },
+  {
+    "revision": "6d5df9f1480fb6ecfa64",
+    "url": "/static/js/2.edd53aa5.chunk.js"
+  },
+  {
+    "revision": "359700b17ed7ece44cf2",
+    "url": "/static/js/main.6ac0c11b.chunk.js"
+  },
+  {
+    "revision": "525efe24670cef16e7d0",
+    "url": "/static/js/runtime~main.0c7f8ab5.js"
+  },
+  {
+    "revision": "7a6bc10028a79f78e424a9c71499fa77",
+    "url": "/static/media/avatar.7a6bc100.jpg"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "6ee66b2cb8a3d9988e37b83b3b4a590e",
+    "url": "/static/media/card-1.6ee66b2c.jpeg"
+  },
+  {
+    "revision": "ae10829f019b0103525a6cd1102ee213",
+    "url": "/static/media/card-2.ae10829f.jpeg"
+  },
+  {
+    "revision": "4ae4eeff542ab32204e8409d170e14a0",
+    "url": "/static/media/card-3.4ae4eeff.jpeg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "f6a5fba36c8960a0bf5693021e1d3039",
+    "url": "/static/media/login_background.f6a5fba3.jpg"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/static/media/logo-white.eec7c7f6.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aba54d652e5df3b9f1f8024cbeb6630f",
+    "url": "/static/media/marc.aba54d65.jpg"
+  },
+  {
+    "revision": "dd31c85ad5252fa4fba82e72c1bdcf9a",
+    "url": "/static/media/netevent_logo_mobile.dd31c85a.svg"
+  },
+  {
+    "revision": "61c4c50e61d68de31c8951af5470a2c2",
+    "url": "/static/media/sidebar-1.61c4c50e.jpg"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/static/media/sidebar-2.d30c9e30.jpg"
+  },
+  {
+    "revision": "c963330bcd48670a75ffdd38622b1532",
+    "url": "/static/media/sidebar-3.c963330b.jpg"
+  },
+  {
+    "revision": "f34e943fbffb73c68e761828b1c00980",
+    "url": "/static/media/sidebar-4.f34e943f.jpg"
+  }
+]);
